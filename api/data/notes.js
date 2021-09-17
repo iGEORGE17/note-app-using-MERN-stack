9dd8644397a215906a7b1dfe76fc40bce9a54445
iGEORGE17 <www.igeorge.www@gmail.com>
2021-09-17 05:23:26 +0100

    initialized the api and client

---
+++ api/data/notes.js
@@ -0,0 +1,22 @@
+const notes = [
+    {
+        _id: "1",
+        title: "Blood is thicker than water",
+        content: "WE were like brothers fro different mothers",
+        category: "Entertainment"
+    },
+    {
+        _id: "2",
+        title: "Big Brother Naija",
+        content: "prize money is now like 90 milion naira",
+        category: "Entertainment"
+    },
+    {
+        _id: "3",
+        title: "Louis Vitton releases new brand",
+        content: "October 28th will be phenomenom in the fahion industry",
+        category: "Fashion"
+    },       
+];
+
+module.exports = notes
